da6b6ff7189250089ea7d3b9fc87d4a4eceaec47
Saravanakrish97 <95554319+Saravanakrish97@users.noreply.github.com>
2024-10-09 14:24:39 +0530

    Update info.py

--- info.py
+++ info.py
@@ -22,9 +22,9 @@ def redirected_env(value):
 
 # Bot information
 SESSION = environ.get('SESSION', 'Media_search')
-API_ID = int(environ['API_ID'])
-API_HASH = environ['API_HASH']
-BOT_TOKEN = environ['BOT_TOKEN']
+API_ID = int(environ('API_ID', '20389440'))
+API_HASH = environ('API_HASH', 'a1a06a18eb9153e9dbd447cfd5da2457')
+BOT_TOKEN = environ('BOT_TOKEN', "")
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
