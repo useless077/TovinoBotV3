36143b775b7432c4a641ea535f422673eb314c9d
useless077 <95554539+useless077@users.noreply.github.com>
2024-10-09 19:03:19 +0530

    Update info.py

--- info.py
+++ info.py
@@ -21,7 +21,7 @@ def redirected_env(value):
         return 'Chat'
 
 # Bot information
-SESSION = environ.get('SESSION', 'Media_search')
+STRING_SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ['API_ID'])
 API_HASH = environ['API_HASH']
 BOT_TOKEN = environ['BOT_TOKEN']
