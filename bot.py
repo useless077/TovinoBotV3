7690ebb66699a3f7e3d82706925662e09acdaf48
useless077 <95554539+useless077@users.noreply.github.com>
2024-10-09 22:34:56 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -21,7 +21,7 @@ class Bot(Client):
 
     def __init__(self):
         super().__init__(
-            session_name=STRING_SESSION,
+            name=STRING_SESSION,
             api_id=API_ID,
             api_hash=API_HASH,
             bot_token=BOT_TOKEN,
