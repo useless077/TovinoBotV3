3da97c780104fa0af24767ff8fcc8b604b1e072f
useless077 <95554539+useless077@users.noreply.github.com>
2024-10-10 16:01:10 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -34,7 +34,6 @@ class Bot(Client):
             workers=50,
             plugins={"root": "plugins"},
             sleep_threshold=5,
-            parse_mode="html",
         )
 
     async def start(self):
